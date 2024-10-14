a72f4e889307147bc16d917c673751edd943ac9d
Eduardo Almeida <almeedusa@gmail.com>
2024-10-14 17:59:18 -0300

    Instanciando de objeto e realizando testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new(numero: "123123", modelo: "ABC123", imei: "11111", memoria: 128);
+Iphone iphone = new(numero: "321312", modelo: "DEF123", imei: "234243", memoria: 256);
+
+iphone.Ligar();
+iphone.InstalarAplicativo(nomeApp: "Whatsapp");
+
+
+nokia.Ligar();
+nokia.InstalarAplicativo(nomeApp: "Facebook");
