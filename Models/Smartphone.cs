1080acf7b417bf89352868826121eaf35d49a462
Eduardo Almeida <almeedusa@gmail.com>
2024-10-14 18:00:28 -0300

    Removendo comentários

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,7 +3,6 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
         protected string Modelo { get; set; }
         protected string Imei { get; set; }
         protected int Memoria { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
 Nokia nokia = new(numero: "123123", modelo: "ABC123", imei: "11111", memoria: 128);
 Iphone iphone = new(numero: "321312", modelo: "DEF123", imei: "234243", memoria: 256);
 
